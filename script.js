611b6f8e49fce5ff74701f778485f7fbd2659f6d
Kiril Dudnik <kdudnik17@gmail.com>
2023-09-05 19:13:09 +0200

    feature: form logic  - add logic for:   + cvs   + submit btn  - wip: spaces between integers in card number

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 
 const nameInput = document.getElementById('nameInput')
 const nameField = document.getElementById('nameField')
+const inputForm = document.querySelector('form')
 
 nameInput.addEventListener('keypress', (event) => {
     const allowedChars = [' ','-','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',]
@@ -26,6 +27,8 @@ nameInput.addEventListener('keyup', () => {
     }
 
     nameField.innerHTML = nameInput.value
+    submitBtn.innerHTML = "Submit"
+    submitBtn.classList.remove('submit--error')
 })
 
 
@@ -51,6 +54,8 @@ cardNumberInput.addEventListener('keypress', (event) => {
 
     numbers.shift()
     numbers.push(event.key)
+    submitBtn.innerHTML = "Submit"
+    submitBtn.classList.remove('submit--error')
     if(cardNumberInput.value.length < 16) {
         cardNumberField.innerHTML = numbers.join("")
     }
@@ -91,26 +96,95 @@ date.addEventListener('keypress', (event) => {
 })
 
 date.addEventListener('keyup', (event) => {
-    const errorEl = date.closest('.form__row').querySelector('.error')
+    const errorEl = date.closest('div').querySelector('.error')
     if(event.target.dataset.error === "true") return;
 
     if(!event.target.value) throwError(errorEl, event.target, "Can't be blank")
 
     const currentField = dateField.querySelector(`[data-date="${event.target.dataset.date}"]`)
     currentField.innerHTML = event.target.value
+    submitBtn.innerHTML = "Submit"
+    submitBtn.classList.remove('submit--error')
     if(currentField.innerHTML.length === 1) {
         currentField.insertAdjacentText('afterbegin', "0")
     }
 })
 
 monthInput.addEventListener('focusout', (event) => {
-    const errorEl = date.closest('.form__row').querySelector('.error')
+    const errorEl = date.closest('div').querySelector('.error')
     if(+monthInput.value > 12) {
         event.preventDefault()
         throwError(errorEl, monthInput, "Please, put the right date")
         return
     }
 })
+
+
+// CVS
+
+
+const cvsField = document.getElementById('cvsField')
+const cvsInput = document.getElementById('cvsInput')
+
+cvsInput.addEventListener('keypress', (event) => {
+    const errorEl = cvsInput.closest('div').querySelector('.error')
+
+    errorEl.style.visibility = "hidden"
+    cvsInput.dataset.error = "false"
+
+    if(!+event.key) {
+        event.preventDefault()
+        throwError(errorEl, cvsInput, "Wrong format, only numbers")
+        return
+    }
+})
+
+cvsInput.addEventListener('keyup', () => {
+    const errorEl = cvsInput.closest('div').querySelector('.error')
+
+    if(cvsInput.dataset.error === "true") return;
+
+    if(!cvsInput.value) throwError(errorEl, cvsInput, "Can't be blank")
+
+    cvsField.innerHTML = cvsInput.value
+    submitBtn.innerHTML = "Submit"
+    submitBtn.classList.remove('submit--error')
+})
+
+
+// SUBMIT
+
+
+const submitBtn = document.querySelector('.btn')
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    inputForm.querySelectorAll('input').forEach(input => {
+        findErrorInInput(input, input.maxLength)
+        if(input.dataset.error === "true") {
+            inputForm.dataset.submitable = "false"
+        }
+    })
+    if(inputForm.dataset.submitable === "false") {
+        submitBtn.innerHTML = "Please check the errors!"
+        submitBtn.classList.add('submit--error')
+        submitBtn.style.animation = "submitError .5s"
+    }
+})
+
+submitBtn.addEventListener('animationend', () => {
+    submitBtn.style.animation = ""
+})
+
+function findErrorInInput(input, symoblsAmount) {
+    const errorEl = input.closest('div').querySelector('.error')
+    if(!input.value) {
+        throwError(errorEl, input, "Can't be blank")
+    }
+    else if(symoblsAmount && input.value.length !== symoblsAmount) {
+        throwError(errorEl, input, `Must contain ${symoblsAmount} symbols`)
+    }
+}
 
 
 // UTILS
